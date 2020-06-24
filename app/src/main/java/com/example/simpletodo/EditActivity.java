3fd0201d69e322796794e4e5ed4bd4dd17f0f981
Jinny Choi <jinchoi99@fb.com>
2020-06-23 23:04:56 -0700

    Going back to MainActivity screen after Save Button

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -2,6 +2,7 @@ package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,11 +34,17 @@ public class EditActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //create an intent which will contain the results
+                Intent intent = new Intent();
 
                 //pass the data (results of editing)
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, etItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
 
                 //set the result of the intent
+                setResult(RESULT_OK, intent);
+
                 //finish activity, close the screen and go back to mainactivity screen
+                finish();
 
 
 
