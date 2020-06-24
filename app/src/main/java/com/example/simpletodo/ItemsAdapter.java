1b03ac07f9eb9c46e9b86d7e991da7d1348b83b3
Jinny Choi <jinchoi99@fb.com>
2020-06-23 18:10:20 -0700

    Implement onBindViewHolder

--- app/src/main/java/com/example/simpletodo/ItemsAdapter.java
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -1,7 +1,9 @@
 package com.example.simpletodo;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,15 +24,19 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
     //responsible for creating each view
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //Use layout inflater to inflate view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
         //wrap it inside View Holder and return it
-
-        return null;
+        return new ViewHolder(todoView);
     }
 
     @Override
-    //responsible for taking data from particular position and putting it into viewholder
+    //responsible for binding data to a particular view holder
+    //responsible for taking data from particular position and putting it into specific view holder
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        //Grab the item at the position
+        String item = items.get(position);
+        //Bing the item to the specific view holder
+        holder.bind(item);
     }
 
     @Override
@@ -42,8 +48,18 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
     //Container to provide easy access to views that represent each row of the list
     class ViewHolder extends RecyclerView.ViewHolder{
 
+        //get reference to the view that we can actually access in bind method
+        TextView tvItem;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            //simple_list_item_1 contains id text1
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        //Update the view inside of the view holder with this data
+        public void bind(String item) {
+            tvItem.setText(item);
         }
     }
 }
