f6ac408134631d5ba60c682ccb67d0047b07b612
Jinny Choi <jinchoi99@fb.com>
2020-06-23 17:48:16 -0700

    Create ViewHoder

--- app/src/main/java/com/example/simpletodo/ItemsAdapter.java
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -1,4 +1,16 @@
 package com.example.simpletodo;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class ItemsAdapter {
+    //Container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
