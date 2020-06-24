b061b59f11a5f542acdd1fe6da57032331a08a39
Jinny Choi <jinchoi99@fb.com>
2020-06-23 17:36:09 -0700

    Add variable from each view

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    //implement model as List of String called items
     List<String> items;
+
+    //Add member variable of each view
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //create mock data
         items = new ArrayList<>();
         items.add("Buy milk");
         items.add("Do laundry");
         items.add("Do homework");
+
+        //get reference of each view in mainActivity
+        //get handle of the btnAdd, etItem, rvItems in mainActivity
     }
 }
