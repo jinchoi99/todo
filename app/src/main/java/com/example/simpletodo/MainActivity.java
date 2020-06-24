f1b17fd26dac907a79b798e73c8ca4b2611755a6
Jinny Choi <jinchoi99@fb.com>
2020-06-23 17:28:07 -0700

    Implement model as List of String

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Do laundry");
+        items.add("Do homework");
     }
 }
